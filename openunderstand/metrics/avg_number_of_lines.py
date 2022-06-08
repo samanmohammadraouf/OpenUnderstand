8ec37ad69a5ea645bc60e3288f604b9bda128922
samanmohammadraouf <saman.raouf@gmail.com>
2022-06-08 13:53:03 +0430

    avg line comments ( not block comments )

--- openunderstand/metrics/avg_number_of_lines.py
+++ openunderstand/metrics/avg_number_of_lines.py
@@ -6,14 +6,16 @@ import argparse
 import os
 
 class Method:
-    def __init__(self,name,numberOfLines,numberOfBlankLines,numberOfCommentLines,numberOfCodeLines):
+    def __init__(self,name,numberOfLines,numberOfBlankLines,numberOfCommentLines,numberOfCodeLines,startLine,endLine):
+        self.startLine =startLine
+        self.endLine = endLine
         self.methodName = name
         self.numberOfLines = numberOfLines
         self.numbderOfBlankLines = numberOfBlankLines
         self.numberOfCommentLines = numberOfCommentLines
         self.numberOfCodeLines = numberOfCodeLines
 
-class FunctionsListener(JavaParserLabeledListener):
+class FunctionsLineListener(JavaParserLabeledListener):
     def __init__(self):
         self.methods = []
 
@@ -21,7 +23,7 @@ class FunctionsListener(JavaParserLabeledListener):
         [FirstLine, col] = str(ctx.start).split(",")[3].split(":")
         col = col[:-1]
         FirstLineNum = int(FirstLine)
-        newMethod = Method(ctx.IDENTIFIER().getText(),FirstLineNum,0,0,0)
+        newMethod = Method(ctx.IDENTIFIER().getText(),FirstLineNum,0,0,0,FirstLineNum,FirstLineNum)
         self.methods.append(newMethod)
 
     def exitMethodBody(self, ctx:JavaParserLabeled.MethodBodyContext):
@@ -31,6 +33,9 @@ class FunctionsListener(JavaParserLabeledListener):
                 col = col[:-1]
                 LastLineNum = int(LastLine)
                 m.numberOfLines = LastLineNum - m.numberOfLines
+                m.endLine = LastLineNum
+
+
 
 def avgMethodsLineNumbers(file_path):
     file_stream = FileStream(file_path)
@@ -39,7 +44,7 @@ def avgMethodsLineNumbers(file_path):
     parser = JavaParserLabeled(token_stream)
     parseTree = parser.compilationUnit()
 
-    listener = FunctionsListener()
+    listener = FunctionsLineListener()
     walker = ParseTreeWalker()
     walker.walk(listener,parseTree)
 
@@ -49,15 +54,39 @@ def avgMethodsLineNumbers(file_path):
     for method in methods:
         SumOfMethodsLines = SumOfMethodsLines + method.numberOfLines
     avgMethodsLines = SumOfMethodsLines / len(methods)
-
     print("avg number of lines of functions :" + str(avgMethodsLines))
 
+    return methods
+
+
+def avgMethodCommentLines(file_path,methods):
+    file_stream = FileStream(file_path)
+    lexer = JavaLexer(file_stream)
+    token_stream = CommonTokenStream(lexer)
+    parser = JavaParserLabeled(token_stream)
+
+    for method in methods:
+        token = lexer.nextToken()
+        while token.line <= method.endLine:
+            if token.line >= method.startLine and token.line <= method.endLine:
+                if token.type == lexer.LINE_COMMENT:
+                    method.numberOfCommentLines = method.numberOfCommentLines +1
+            token = lexer.nextToken()
+
+    SumOfMethodsCommentLines = 0
+    for method in methods:
+        SumOfMethodsCommentLines = SumOfMethodsCommentLines + method.numberOfCommentLines
+    avgMethodsCommentsLines = SumOfMethodsCommentLines / len(methods)
+
+    print("avg number of comments lines of functions :" + str(avgMethodsCommentsLines))
+
 if __name__ == '__main__':
+
    for dirpath, dirnames, filenames in os.walk("D:/university/Term6/Courses/Compiler/Project_phase_2/OpenUnderstand-master/benchmark/metricsTest"):
         for filename in [f for f in filenames if f.endswith(".java")]:
                 print("for file :"+ filename)
                 file_path = os.path.join(dirpath,filename)
-                avgMethodsLineNumbers(file_path)
-                #avgMethodsCommentLines(file_path)
-                #avgMethodsBlankLines(file_path)
-                #avgMethodsCodeLines(file_path)
+                methods = avgMethodsLineNumbers(file_path)
+                avgMethodCommentLines(file_path,methods)
+                #avgMethodCodeLines(file_path , methods)
+                #avgMethodsBlankLines(file_path,methods)
